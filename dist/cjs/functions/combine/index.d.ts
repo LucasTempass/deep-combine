ada7500c4425a8f8798d44bbf6ef40ade90d0c30
Lucas Tempass <lucas.tempass@hotmail.com>
2023-07-03 20:59:49 -0300

    🔨 Configures husky pre-push

--- dist/cjs/functions/combine/index.d.ts
+++ dist/cjs/functions/combine/index.d.ts
@@ -1,6 +1,2 @@
 import { DeepPartial, NonEmptyArray } from "../../types";
-
-export declare function combine<
-  Target extends object = object,
-  O extends Target = Target
->(original: Target, ...objects: NonEmptyArray<DeepPartial<Target>>): Target;
+export declare function combine<Target extends object = object, O extends Target = Target>(original: Target, ...objects: NonEmptyArray<DeepPartial<Target>>): Target;
